216b3afc3e239f7af2e876da9a9ef88aa3e19070
USER <18-36187-1@student.aiub.edu>
2021-08-02 09:35:30 +0600

    lab 5

---
+++ lab5/index.php
@@ -0,0 +1,22 @@
+<script >
+	function get(id)
+	{
+		return document.getElementById(id);
+	}
+	function loadDoc()
+	{
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET","myprofile.php",true);
+		xhr.onreadystatechange=function()
+		{
+			if (this.readyState==4 && this.status == 200) {
+				get("demo").innerHTML = this.responseText;
+			}
+		};
+		xhr.send();
+	}
+</script>
+<button onclick="loadDoc()">
+	Click me
+</button>
+<div id= "demo"></div>
---
+++ lab5/myprofile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<?php $name-"Labony" ;?>
+<body>
+<h1 align="center">MY Profile</h1>
+<table align="center">
+	<tr>
+		<td><?php  echo $name;?></td>
+	</tr>
+	
+</table>
+</body>
+</html>
